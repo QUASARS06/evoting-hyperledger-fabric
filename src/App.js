a79d5a21f2f07f64ea69de5f0a079b04045fe8b1
chiragjain <chirag.jain@spit.ac.in>
2021-11-05 16:06:34 +0530

    login & register done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <Router>
+      <div className='App'>
+        <div className='container'>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/pages/Register.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+
+const theme = createTheme();
+
+const Register = () => {
+  const [username, setUsername] = React.useState('');
+  const [org, setOrg] = React.useState('org1');
+
+  const handleChange = (event, newAlignment) => {
+    setOrg(newAlignment);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // eslint-disable-next-line no-console
+    console.log({
+      username,
+      org,
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Register With Organization
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='username'
+              label='Username'
+              autoFocus
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <br />
+
+            <ToggleButtonGroup
+              color='primary'
+              exclusive
+              fullWidth
+              value={org}
+              onChange={handleChange}
+            >
+              <ToggleButton value='org1'>Organization 1</ToggleButton>
+              <ToggleButton value='org2'>Organization 2</ToggleButton>
+            </ToggleButtonGroup>
+
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Register
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default Register;
